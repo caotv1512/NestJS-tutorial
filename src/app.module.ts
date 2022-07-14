a7798fef1018adf707098714c6b6c6547ebc20c8
DESKTOP-H3TDTSK\Lenovo <cao.tran@sotatek.com>
2022-07-14 17:50:51 +0700

    Feat: Add typeorm to app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './../ormconfig';
 
 @Module({
-  imports: [TodosModule],
-  controllers: [AppController, UsersController],
+  imports: [TodosModule, TypeOrmModule.forRoot(config)],
+controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
 export class AppModule {}
